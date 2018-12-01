bbcd28e9fdd182ccc0ae2288a3cc011d173ca2d0
Akshay Krishna <ak20@princeton.edu>
2018-12-01 15:27:40 -0500

    added identity model

--- models.py
+++ models.py
@@ -1,9 +1,22 @@
-def Yamadaneuron(x, beta, ....):
+def Yamada(x, beta):
     #define function equations
     pass
 
-def FitzHughNeuron():
-    pass
+def FitzHughNagumo(x, y, a=1.0, b=1.0, tau=1.0):
+    """
+    The y's are phase space variables (outputs), the x is the scalar input
+    d y1 / dt = y1 - y1^3/3 - y2 + x
+    d y2 / dt = (y1 + a - b*y2) / tau
+    parameters: a, b, tau
+    """
+    return np.array([y[0] - y[0]**3/3.0 - y[1] + x,
+                     (y[0] + a - b*y[1]) / tau ])
 
-def identity():
-    # should return output = input
+def identity(x, y, h):
+    """
+    should return output = input
+    y_n+1 = x_n, or alternatively
+    dy / dt = (x-y) / h
+    where h is the Euler time step
+    """
+    return (x-y) / h
--- neuron.py
+++ neuron.py
@@ -9,33 +9,44 @@ class Neuron(object):
     Initialize it as 
     myneuron = Neuron(params)
     where params is a dict with parameter values, for example
-    params = {'x0': 0.0, 'model': }
+    params = {'model': "identity", 'x0': 0.0}
 
     """
     
     def __init__(self, params):
         # constructor to initialize a Neuron
-        self.x0 = params.get("x0", 0.0) # initial state
-        self.model = params.get("model", "FitzHughNagamo")
+        self.model = params.get("model", "identity")
         self.dt = params.get("dt", 1.0e-6)
-        self.y = 0.0 # output
-        self.history_len = params.get("dt", 1.0e-6)
+        self.dim # number of dimensions of phase space
+        self.y
+        self.history_len = params.get("history_len", 10)
 
 
         # set model ...
-        if self.model == 'FitzHughNagamo':
+        if self.model == 'identity':
+            self.dim = 1
+            self.f = models.identity
+            
+
+
+        elif self.model == 'FitzHughNagumo':
+            self.x0 = params.get("x0", 0.0) # initial state
             # read in the parameters, do some stuff
-            self.f = models.FitzHughNagamo(...)
-            pass
+            self.f = models.FitzHughNagamo
 
         elif self.model == 'Yamada':
-            # do sonething else
-            pass
+            self.x0 = params.get("x0", 0.0) # initial state
+            self.f = models.Yamada
 
         else:
-            # raise an exception (not implemented)
-            pass 
+            raise ValueError("Not implemented")
 
+
+        self.y = params.get("initial_state", np.zeros(self.dim)) # default initial state, all zeros
+        if len(self.y) != self.dim
+            raise ValueError(
+                "The initial state has {0:d} dimensions but the {1:s} model has a {2:d}-dim phase space".
+                format(len(self.y), self.model, self.dim))
 
         # set solver
         if self.solver == 'Euler':
@@ -56,6 +67,14 @@ class Neuron(object):
     def step(self, x):
         """ get the output at the next time step, given an input x
             update history ...
+            y_{n+1} = y_n + h f(x_n, y_n)
         """
-        return x
+        y = x
+        return y # return output y (t+dt)
+
+    def solve(self, x):
+        """ get the entire output time series of a neuron with input time series x
+        """
+        pass
+
         #pass
