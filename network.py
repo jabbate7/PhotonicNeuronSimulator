d805301142e7bdbe32c8c142edafd5cd2e8ed322
Joe Abbate <joseph.abbate7@gmail.com>
2018-12-01 15:24:42 -0500

    Add graphics file with images of a simple and complex network

--- network.py
+++ network.py
@@ -1,5 +1,18 @@
+from neuron import Neuron
+from scipy.sparse import csr_matrix
+
 # A network is a specific topology of Neurons
 class Network:
+    """
+    This is a Network object
+    Initiatlize it as 
+    mynetwork = Network(neurons, weights, delays, dt)
+    neurons is an array of Neuron objects
+    weights is a matrix of weights
+    delays is a matrix of time delays
+    dt is the time step for propagating the network
+    see the readme for structure of "weights" and "delays"
+    """
     def __init__(self, neurons, weights, delays, dt):
         self.neurons=neurons
         self.weights=weights
@@ -12,9 +25,10 @@ class Network:
         pass
         
 # List of Neuron objects
-neurons=[1,2,3]
+neur_1=Neuron({'x0': 0, })
+neurons=[neur_1,neur_2,neur_3]
 # Weight matrix
-# 1 goes to 2, 2 goes to 3, 3 goes to output
+# Input goes 1, 1 goes to 2, 2 goes to 3, 3 goes to output
 W=[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
 # Time delay matrix
 # No time delays
