8353bfd9cff8dffe279bf977db13d6cc64b356f2
Akshay Krishna <ak20@princeton.edu>
2018-12-11 10:25:14 -0500

    replace append with insert

--- neuron.py
+++ neuron.py
@@ -143,7 +143,7 @@ class Neuron(object):
         k2 = 0
         self.y = self.y + self.dt * self.f(x, self.y)
 
-        self.hist.append(self.y.copy())
+        self.hist.insert(0, self.y.copy())
         # trim the history from the back if it grows too big
         if len(self.hist) > self.hist_len: 
             _ = self.hist.pop()
