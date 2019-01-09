1e53a87efc033d62030f595d99812b5cb47de3c6
Gerry Angelatos <ga4@princeton.edu>
2019-01-09 14:20:03 -0500

    test removing docstring from visualize_plot

--- network.py
+++ network.py
@@ -309,7 +309,6 @@ class Network:
             A 3D array (num_timesteps X num_neuron X neuron.dim)
              of the full state of each neuron in the network at each time.
         """
-        #skeleton version for now
         Len_t=external_inputs.shape[0]#first dimension is time
         msg="External input matrix needs shape ({}, {})".format(Len_t, self.num_inputs)
         assert (external_inputs.shape[1]==int(self.num_inputs)), msg
@@ -322,33 +321,7 @@ class Network:
         return Net
 
     def visualize_plot(self, inputs, outputs, time=None, plotparams=None):
-        """
-        Generate a simple and easy to read plot of the network dynamics. 
 
-        After solving a network for a given set of inputs, pass these inputs
-        and the computed result from network_solve to generate a plot. 
-        Use returned figure handle to update plot parameters from defaults 
-        if desired.
-
-        Parameters
-        -----------
-        time
-            an array of time points which inputs and outputs are plotted over
-        inputs
-            the 2D array (num_timesteps X num_neurons) array of total inputs
-             to each neuron in the network
-        outputs
-            the 2D array (num_timesteps X num_neurons) of the state of each 
-            neuron in the network as a function of time
-        plotparams
-            A dictionary of plot parameters to be used
-
-        Returns
-        ----------
-        np.array
-            A 3D array (num_timesteps X num_neuron X neuron.dim)
-             of the full state of each neuron in the network at each time.
-        """
 
         #havent implimented default parameters yet, sue me
 
